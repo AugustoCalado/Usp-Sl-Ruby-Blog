a3165290ee64c6971204a7f0fd360a2991435152
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2019-10-03 22:58:11 -0300

    Finished topic 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'welcome/index' #ells Rails to map requests to http://localhost:3000/welcome/index to the welcome controller's index action
+
+  root 'welcome#index' #tells Rails to map requests to the root of the application to the welcome controller's
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
