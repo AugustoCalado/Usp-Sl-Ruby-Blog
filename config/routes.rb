8a78fcbd53b577dce6e67e6878f1b20befb5f17e
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2019-10-05 21:30:26 -0300

    adding aa route for comments - so that rails knows how to navigate to these comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index' #ells Rails to map requests to http://localhost:3000/welcome/index to the welcome controller's index action
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   root 'welcome#index' #tells Rails to map requests to the root of the application to the welcome controller's
 
